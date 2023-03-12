8bc5cd55de648a331f99aff1103ec5a2abde9048
Pedro <pedro@zibi.app>
2023-03-12 11:47:35 -0500

    Getting audio frequencies from audio file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swapper-audiowmark",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,13 +15,19 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.4"),
+        .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.5.2")
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swapper-audiowmark",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "AudioKit", package: "AudioKit"),
+            ]),
         .testTarget(
             name: "swapper-audiowmarkTests",
             dependencies: ["swapper-audiowmark"]),
---
+++ Sources/swapper-audiowmark/utils.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexis Rodriguez on 3/12/23.
+//
+
+import Foundation
